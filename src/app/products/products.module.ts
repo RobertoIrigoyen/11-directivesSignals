759be826aed5f86f7cd364dd4497ad8525463878
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2024-02-26 14:57:51 +0100

    inicio directivas personalizadas

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductPageComponent } from './product-page/product-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+  
+    ProductPageComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    ProductsRoutingModule
+  ]
+})
+export class ProductsModule { }
